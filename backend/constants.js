0ac78664b56b4859175c057cf87bf8c3d056281b
test <test@gmail.com>
2021-02-06 19:37:32 +0100

    Test commit

---
+++ backend/constants.js
@@ -0,0 +1,9 @@
+const FRAME_RATE = 10
+const GRID_SIZE = 20
+const FOOD_TYPES = ['NORMAL', 'POISON', 'SUPER', 'SUPER_POWER']
+
+module.exports = {
+    FRAME_RATE,
+    GRID_SIZE,
+    FOOD_TYPES
+}
