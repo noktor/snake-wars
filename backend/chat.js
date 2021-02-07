8f9e99a08007054310645921b02d584e0df0c7d3
Albert Vila <avilac4@gmail.com>
2021-02-07 10:10:13 +0100

    prova websockets join create room

---
+++ backend/chat.js
@@ -0,0 +1,164 @@
+const { GRID_SIZE, FOOD_TYPES } = require('./constants')
+
+module.exports = {
+    initChat,
+    gameLoop,
+    getUpdatedVelocity
+}
+
+function initChat() {
+    const state = createChatState()
+    return state
+}
+
+function createChatState() {
+    return {
+        members: []
+    }
+}
+
+function createGameState() {
+    return {
+        players: [{
+            playerId: 1,
+            pos: {
+                x: 3,
+                y: 5,
+            },
+            vel: {
+                x: 0,
+                y: 0
+            },
+            snake: [
+                {x: 1, y: 5},
+                {x: 2, y: 5},
+                {x: 3, y: 5},
+            ]
+        },
+        {
+            playerId: 2,
+            pos: {
+                x: 18,
+                y: 15,
+            },
+            vel: {
+                x: 0,
+                y: 0
+            },
+            snake: [
+                {x: 20, y: 15},
+                {x: 19, y: 15},
+                {x: 18, y: 15},
+            ]
+        }],
+        foodList: [],
+        gridSize: GRID_SIZE
+    }
+}
+
+function gameLoop(state) {
+    if(!state) {
+        return
+    }
+
+    for(let player of state.players) {
+        player.pos.x += player.vel.x
+        player.pos.y += player.vel.y        
+    }
+
+    return processPlayerSnakes(state)
+}
+
+function processPlayerSnakes(state) {
+    for(let player of state.players) {
+        if(player.pos.x < 0 || player.pos.x > GRID_SIZE || player.pos.y < 0 ||player.pos.y > GRID_SIZE) {
+            return player.playerId 
+        }
+    
+        for(let food in state.foodList) {
+            if(state.foodList[food].x === player.pos.x && state.foodList[food].y === player.pos.y) {
+                switch(state.foodList[food].foodType) {
+                    case FOOD_TYPES[2]:
+                        player.snake.push({...player.pos})
+                        player.snake.push({...player.pos})
+                    case FOOD_TYPES[0]:
+                        player.snake.push({...player.pos})
+                        player.pos.x += player.vel.x
+                        player.pos.y += player.vel.y        
+                        break
+                    case FOOD_TYPES[1]:
+                        player.snake.shift({...player.pos})
+                        break
+                }
+                state.foodList.splice(food, 1)
+                randomFood(state)
+                break
+            }
+        }
+    
+        if(player.vel.x || player.vel.y) {
+            for(let p of state.players){ 
+                for(let cell of p.snake) {
+                    if(cell.x === player.pos.x && cell.y === player.pos.y) {
+                        return player.playerId
+                    }
+                }
+            }
+            player.snake.push({...player.pos})
+            player.snake.shift()
+        }
+    }
+
+    let genRandomFood = Math.random() * 100
+    if(genRandomFood <= 2.5)  randomFood(state)
+
+    return false
+}
+
+function randomFood(state) {
+    food = {
+        foodType: generateFoodType(),
+        x: Math.floor(Math.random() * GRID_SIZE),
+        y: Math.floor(Math.random() * GRID_SIZE)
+    }
+
+    for(let player of state.players){
+        for(let cell of player.snake) {
+            if(cell.x === food.x && cell.y === food.y) {
+                return randomFood(state)
+            }
+        }
+    }
+
+    for(let food2 of state.foodList) {
+        if(food2.x === food.x && food2.y === food.y) {
+            return randomFood(state)
+        }
+    }
+
+    state.foodList.push(food)
+}
+
+function generateFoodType() {
+    let randomNumber = Math.floor(Math.random() * 100)
+    if(randomNumber >= 51) return FOOD_TYPES[0]
+    if(randomNumber >= 21) return FOOD_TYPES[1]
+    if(randomNumber >= 1) return FOOD_TYPES[2]
+}
+
+function getUpdatedVelocity(previousVel, keyCode) {
+    switch(keyCode) {
+        case 37: // left
+            return (previousVel.x !== 1) ? { x: -1, y: 0 } : { x: 1, y: 0 }
+            break
+        case 38: // down
+            return (previousVel.y !== 1) ? { x: 0, y: -1 } : { x: 0, y: 1 }
+            break
+        case 39: // right
+            return (previousVel.x !== -1) ? { x: 1, y: 0 } : { x: -1, y: 0 }
+            break
+        case 40: // left
+            return (previousVel.y !== -1) ? { x: 0, y: 1 } : { x: 0, y: -1 }
+            break                        
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+const { makeId } = require('./utils')
+const { initChat } = require('./chat')
+
 const WebSocket = require('ws')
 
 const wss = new WebSocket.Server({ port: process.env.PORT || 3000 },()=>{
@@ -5,13 +8,81 @@ const wss = new WebSocket.Server({ port: process.env.PORT || 3000 },()=>{
     console.log(process.env.PORT)
 })
 
-wss.on('connection', function connection(ws) {
-   ws.on('message', (data) => {
+const chats = {}
+const clientRooms = {}
+const rooms = {}
+
+wss.on('connection', socket => {
+   const uuid = socket.id
+   socket.on('message', (data) => {
       console.log('data received \n %o',data)
-      ws.send('Bon dia pel matí a la vila del pingüí');
+      socket.send('Bon dia pel matí a la vila del pingüí')
    })
+
+   //socket.on('createChatRoom', handleCreateChatRoom)
+   socket.on('joinChatRoom', handleCreateChatRoom)
+
+   console.log("CLIENT CONNECTED")
+   console.log(client.id)
+
+   socket.on("close", () => {
+      // for each room, remove the closed socket
+      console.log("deleted rooms")
+      Object.keys(rooms).forEach(room => leave(room));
+      console.log(rooms)
+   });
+
+   function handleCreateChatRoom() {
+         // const { message, meta, room } = data;
+      
+         if(! rooms['room1']) {
+            console.log("create room")
+           if(! rooms['room1']) rooms['room1'] = {}; // create the room
+           if(! rooms['room1'][uuid]) rooms['room1'][uuid] = socket; // join the room
+           Object.entries(rooms['room1']).forEach(([, sock]) => sock.send( 'Missatge de prova CREACIO' ));
+         } else {
+            console.log("join room")
+           // send the message to all in the room
+           Object.entries(rooms['room1']).forEach(([, sock]) => sock.send( 'Missatge de prova JOIN' ));
+         }
+
+         console.log(rooms)
+         // if(Object.keys(clientRooms).length === 0) {
+         //    let privateChatName = makeId(5)
+         //    clientRooms[client.id] = privateChatName
+         //    //  client.emit('privateChatCode', privateChatName)
+         //    //  chats[privateChatName] = initGame()
+         //    client.join(privateChatName)
+         // } else {
+         //    client.join(Object.keys(clientRooms)[0])
+
+         //    const room = io.sockets.adapter.rooms[Object.keys(clientRooms)[0]]
+
+         //    let allUsers
+     
+         //    if(room) {
+         //        allUsers = room.sockets
+         //    }
+     
+         //    let numClients = 0
+         //    if(allUsers) {
+         //        numClients = Object.keys(allUsers).length
+         //    }
+     
+         //    //room.sockets.
+
+         // }
+         
+      // }
+      //  client.number = 1
+      //  client.emit('init', 1)
+   }
+
 })
 
 wss.on('listening',()=>{
    console.log('listening on 8080')
 })
+
+
+
