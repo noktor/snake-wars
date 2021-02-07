8df4e5eb7019d09b7bdbd0ba6881a7926513582b
Albert Vila <avilac4@gmail.com>
2021-02-07 09:14:38 +0100

    test stuff

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 const WebSocket = require('ws')
+
 const wss = new WebSocket.Server({ port: process.env.PORT || 3000 },()=>{
     console.log('server started')
     console.log(process.env.PORT)
 })
+
 wss.on('connection', function connection(ws) {
    ws.on('message', (data) => {
       console.log('data received \n %o',data)
-      ws.send(data);
+      ws.send('Bon dia pel matí a la vila del pingüí');
    })
 })
+
 wss.on('listening',()=>{
    console.log('listening on 8080')
 })
